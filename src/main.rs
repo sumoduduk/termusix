a2ad70fee45b62f7cb6ea8a65ab9c5e35764c343
sumoduduk <sumoduduk13@gmail.com>
2024-07-17 21:27:49 +0700

    fix: return receiver channel instead result

--- src/main.rs
+++ src/main.rs
@@ -81,14 +81,14 @@ async fn main() -> eyre::Result<()> {
             let terminal = Terminal::new(backend)?;
             let events = event::EventHandler::new(250);
             let mut tui = tui::Tui::new(terminal, events);
-            tui.init();
+            tui.init()?;
 
             // Start the main loop.
             while app.running {
                 // Render the user interface.
                 tui.draw(&mut app)?;
                 // Handle events.
-                match tui.events.next() {
+                match tui.events.next().await? {
                     Event::Tick => app.tick(),
                     Event::Key(key_event) => handle_key_events(key_event, &mut app)?,
                     Event::Mouse(_) => {}
