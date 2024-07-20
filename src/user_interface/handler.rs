48c50e090bc2b659187e29a9a088e1b7089d2e8c
sumoduduk <sumoduduk13@gmail.com>
2024-07-20 12:30:14 +0700

    feat(handler) : add keymap j and k

--- src/user_interface/handler.rs
+++ src/user_interface/handler.rs
@@ -1,6 +1,8 @@
 use crate::app::{App, AppResult};
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 
+use super::app::screen::Screen;
+
 /// Handles the key events and updates the state of [`App`].
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
     match key_event.code {
@@ -21,6 +23,25 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
         KeyCode::Left => {
             app.decrement_counter();
         }
+
+        KeyCode::Tab => {
+            app.next_screen();
+        }
+
+        KeyCode::Char('j') => match app.screen_state {
+            Screen::Playback => {}
+            Screen::Playlist => app.next_tab(),
+            Screen::ListMusic => {
+                app.music_list.list_state.select_next();
+            }
+        },
+        KeyCode::Char('k') => match app.screen_state {
+            Screen::Playback => {}
+            Screen::Playlist => app.prev_tab(),
+            Screen::ListMusic => {
+                app.music_list.list_state.select_previous();
+            }
+        },
         // Other handlers you could add here.
         _ => {}
     }
