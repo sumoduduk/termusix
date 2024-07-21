ea38a68f2ae8b72a56725bb0711fd42878845978
sumoduduk <sumoduduk13@gmail.com>
2024-07-21 13:41:22 +0700

    feat: add another sender at initialize app

--- src/user_interface/app.rs
+++ src/user_interface/app.rs
@@ -1,3 +1,5 @@
+use std::path;
+
 use music_list::MusicList;
 use ratatui::{
     buffer::Buffer,
@@ -6,8 +8,9 @@ use ratatui::{
     widgets::{ListState, Widget},
 };
 use screen::{get_border_color, Screen};
+use tokio::sync::mpsc::Sender;
 
-use crate::playlist::Playlist;
+use crate::{playback::PlaybackEvent, playlist::Playlist};
 
 mod music_list;
 pub mod screen;
@@ -24,32 +27,30 @@ pub type AppResult<T> = eyre::Result<T>;
 pub struct App {
     /// Is the application running?
     pub running: bool,
-    pub counter: u8,
-    pub music_list: MusicList,
+    pub music_list: ListState,
     pub screen_state: Screen,
     pub playlist: Playlist,
     pub tabs_playlist: ListState,
+    pub tx_playback: Sender<PlaybackEvent>,
 }
 
-impl Default for App {
-    fn default() -> Self {
+// impl Default for App {
+//     fn default() -> Self {}
+// }
+
+impl App {
+    /// Constructs a new instance of [`App`].
+    pub fn new(tx: Sender<PlaybackEvent>) -> Self {
         let playlist = Playlist::new().expect("ERROR: No playlist found");
 
         Self {
             running: true,
-            counter: 0,
-            music_list: MusicList::default(),
+            music_list: ListState::default(),
             screen_state: Screen::default(),
             playlist,
             tabs_playlist: ListState::default(),
+            tx_playback: tx,
         }
-    }
-}
-
-impl App {
-    /// Constructs a new instance of [`App`].
-    pub fn new() -> Self {
-        Self::default()
     }
 
     /// Handles the tick event of the terminal.
@@ -58,40 +59,6 @@ impl App {
     /// Set running to false to quit the application.
     pub fn quit(&mut self) {
         self.running = false;
-    }
-
-    pub fn increment_counter(&mut self) {
-        if let Some(res) = self.counter.checked_add(1) {
-            self.counter = res;
-        }
-    }
-
-    // pub fn next_tab(&mut self) {
-    //     let len = self.playlist.list_playlist_titles().len();
-    //     let i = self.tabs_playlist;
-    //
-    //     if i >= len {
-    //         self.tabs_playlist = 0;
-    //     } else {
-    //         self.tabs_playlist += 1;
-    //     }
-    // }
-    //
-    // pub fn prev_tab(&mut self) {
-    //     let len = self.playlist.list_playlist_titles().len();
-    //     let i = self.tabs_playlist;
-    //
-    //     if i == 0 {
-    //         self.tabs_playlist = len - 1;
-    //     } else {
-    //         self.tabs_playlist -= 1;
-    //     }
-    // }
-
-    pub fn decrement_counter(&mut self) {
-        if let Some(res) = self.counter.checked_sub(1) {
-            self.counter = res;
-        }
     }
 
     pub fn next_screen(&mut self) {
@@ -108,10 +75,26 @@ impl App {
         get_border_color(&self.screen_state, screen)
     }
 
-    pub fn list_playlist_music(&mut self, indx: Option<usize>) {
+    pub fn list_playlist_music(&self, indx: Option<usize>) -> Option<Vec<String>> {
         let music_l = self.playlist.list_music_by_idx(indx);
         if let Some(list) = music_l {
-            self.music_list.append_music(list.into_values().collect())
+            let s: Vec<_> = list.into_values().collect();
+            Some(s)
+        } else {
+            None
+        }
+    }
+
+    pub async fn play_music(&self) {
+        let idx = self.tabs_playlist.selected().unwrap_or(0);
+
+        if let Some(playlist) = self.playlist.list_music_by_idx(Some(idx)) {
+            let data: Vec<String> = playlist
+                .keys()
+                .map(|id| ["music/", id, ".mp3"].concat())
+                .collect();
+
+            let _ = self.tx_playback.send(PlaybackEvent::Playlist(data)).await;
         }
     }
 }
