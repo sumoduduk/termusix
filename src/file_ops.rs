09959af7b0ba54f6101212f695b2c130628f3953
sumoduduk <sumoduduk13@gmail.com>
2024-07-22 16:14:06 +0700

    fix: change size to len so it can compile in windows

--- src/file_ops.rs
+++ src/file_ops.rs
@@ -1,5 +1,3 @@
-use std::os::unix::fs::MetadataExt;
-
 use std::fs::metadata as metada_sync;
 use tokio::fs::metadata;
 
@@ -7,7 +5,7 @@ pub async fn check_file_exist(file_name: &str) -> Option<&str> {
     let meta_res = metadata(file_name).await;
     match meta_res {
         Ok(meta_res) => {
-            let size = meta_res.size();
+            let size = meta_res.len();
             let is_file = meta_res.is_file();
 
             if size > 0 && is_file {
@@ -24,7 +22,7 @@ pub fn check_file_exist_sync(file_name: &str) -> Option<&str> {
     let meta_res = metada_sync(file_name);
     match meta_res {
         Ok(meta_res) => {
-            let size = meta_res.size();
+            let size = meta_res.len();
             let is_file = meta_res.is_file();
 
             if size > 0 && is_file {
@@ -36,3 +34,17 @@ pub fn check_file_exist_sync(file_name: &str) -> Option<&str> {
         Err(_) => None,
     }
 }
+
+// pub async fn count_file(folder_path: &str) -> eyre::Result<usize> {
+//     let mut count = 0;
+//
+//     let mut dir = fs::read_dir(folder_path).await?;
+//     while let Some(entry) = dir.next_entry().await? {
+//         let file_type = entry.file_type().await?;
+//         if file_type.is_file() {
+//             count += 1;
+//         }
+//     }
+//
+//     Ok(count)
+// }
