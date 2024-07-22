757c9b21a60c059d8aeb9ada281e6225bce25ccd
sumoduduk <sumoduduk13@gmail.com>
2024-07-22 16:48:35 +0700

    chore(handler) : import play and download function in fucntion

--- src/user_interface/handler.rs
+++ src/user_interface/handler.rs
@@ -1,9 +1,12 @@
+mod play;
+
 use crate::{
     app::{App, AppResult},
     download::download_music,
     playback::PlaybackEvent,
 };
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use play::play_and_download;
 
 use super::app::screen::Screen;
 
@@ -50,30 +53,7 @@ pub async fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<
         #[allow(clippy::single_match)]
         KeyCode::Char('p') => match app.screen_state {
             Screen::Playlist => {
-                // app.play_music();
-
-                let indx = app.tabs_playlist.selected();
-                let send = app.tx_playback.clone();
-                if let Some((list, len)) = app.list_id_downloaded_first(indx) {
-                    tokio::spawn(async move {
-                        let downloaded: Vec<String> = list[..len]
-                            .to_vec()
-                            .iter()
-                            .map(|id| concat_file(id))
-                            .collect();
-
-                        if !downloaded.is_empty() {
-                            let _ = send.send(PlaybackEvent::Playlist(downloaded));
-                        }
-                        let not_downloaded = &list[len..].to_vec();
-                        for id in not_downloaded {
-                            let res = download_music(id).await;
-                            if res.is_ok() {
-                                let _ = send.send(PlaybackEvent::Add(concat_file(id)));
-                            }
-                        }
-                    });
-                }
+                play_and_download(app).await;
             }
             _ => {}
         },
@@ -93,8 +73,4 @@ pub async fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<
         _ => {}
     }
     Ok(())
-}
-
-fn concat_file(id: &str) -> String {
-    ["music", id, ".mp3"].concat()
 }
