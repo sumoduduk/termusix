62725c38e3e80832707edb02ae2ac5b50febfc6d
sumoduduk <sumoduduk13@gmail.com>
2024-07-27 12:35:30 +0700

    feat(ui) : add pop up area component to render pop up

--- src/user_interface/app/ui.rs
+++ src/user_interface/app/ui.rs
@@ -2,18 +2,21 @@ mod footer_components;
 mod music_list_components;
 mod playback_component;
 mod playlist_components;
+mod pop_up;
 
 use footer_components::render_footer;
 use music_list_components::render_music_list;
 use playback_component::render_playback;
 use playlist_components::render_playlist;
+use pop_up::centered_rect;
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Layout, Rect},
     style::{palette::tailwind::SLATE, Modifier, Style},
+    widgets::{Block, Clear, Widget},
 };
 
-use super::App;
+use super::{screen::Screen, App};
 use Constraint::*;
 
 const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
@@ -32,4 +35,17 @@ pub fn render(app: &mut App, area: Rect, buf: &mut Buffer) {
     render_playlist(app, playlist_layout, buf);
     render_music_list(app, music_list_layout, buf);
     render_footer(app, footer_layout, buf);
+
+    #[allow(clippy::single_match)]
+    match app.screen_state {
+        Screen::InsertPlaylist => {
+            let pop_up_area = centered_rect(60, 20, area);
+
+            Clear.render(pop_up_area, buf);
+            Block::bordered()
+                .title("Insert Playlist")
+                .render(pop_up_area, buf);
+        }
+        _ => {}
+    }
 }
