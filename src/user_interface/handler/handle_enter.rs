0f5ad5c917b4707f9e8024c5e46370c7d60eb9b1
sumoduduk <sumoduduk13@gmail.com>
2024-07-28 05:21:46 +0700

    fix: fix insert input enter key

--- src/user_interface/handler/handle_enter.rs
+++ src/user_interface/handler/handle_enter.rs
@@ -2,26 +2,9 @@ use crate::app::{screen::Screen, App};
 
 pub async fn enter_key(app: &mut App) {
     match app.screen_state {
-        Screen::InsertPlaylist => {
-            ui_save_playlit(app).await;
-        }
         Screen::Playlist => {
             app.screen_state = Screen::ListMusic;
         }
         _ => {}
     }
-}
-
-async fn ui_save_playlit(app: &mut App) {
-    let playlist_input = app.input_playlist.value();
-
-    if playlist_input.contains("youtube") && playlist_input.contains("playlist") {
-        let _ = app.playlist.save_playlist(playlist_input).await;
-    } else {
-        let _ = app.playlist.save_local_playlist(playlist_input);
-    }
-
-    app.input_playlist.reset();
-    app.tabs_playlist.select_last();
-    app.screen_state = Screen::ListMusic;
 }
