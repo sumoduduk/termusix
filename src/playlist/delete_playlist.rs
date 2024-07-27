953ac012c904adefc21a9902b161c0eff311fd54
sumoduduk <sumoduduk13@gmail.com>
2024-07-27 17:52:31 +0700

    feat: function to delete playlist and music

---
+++ src/playlist/delete_playlist.rs
@@ -0,0 +1,13 @@
+use super::Playlist;
+
+pub fn remove_playlist(playlist: &mut Playlist, index: usize) -> Option<()> {
+    playlist.0.swap_remove_index(index)?;
+    Some(())
+}
+
+pub fn remove_song(playlist: &mut Playlist, index_id: usize, index_song: usize) -> Option<()> {
+    let playlist_info = playlist.0.get_index_mut(index_id)?;
+    playlist_info.1.music_list.swap_remove_index(index_song)?;
+
+    Some(())
+}
