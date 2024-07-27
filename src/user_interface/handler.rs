78ee262190acd6fd8eb4d152c679bd363dd73f5c
sumoduduk <sumoduduk13@gmail.com>
2024-07-28 05:24:31 +0700

    fix: separate handle key event for input playlist

--- src/user_interface/handler.rs
+++ src/user_interface/handler.rs
@@ -1,17 +1,19 @@
 mod handle_d;
 mod handle_enter;
 mod handle_up_down;
+pub mod input_playlist_handler;
+mod insert_playlist_song;
 mod play;
 
 use crate::app::{App, AppResult};
-use crossterm::event::{Event, KeyCode, KeyEvent, KeyModifiers};
+use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
 use handle_d::handle_delete_key;
 use handle_enter::enter_key;
 use handle_up_down::{handle_key_down, handle_key_up};
+use insert_playlist_song::handle_a;
 use play::play_and_download;
 
 use super::app::screen::Screen;
-use tui_input::backend::crossterm::EventHandler;
 
 /// Handles the key events and updates the state of [`App`].
 pub async fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
@@ -21,10 +23,9 @@ pub async fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<
             app.quit();
         }
 
-        KeyCode::Esc => match app.screen_state {
-            Screen::InsertPlaylist => app.screen_state = Screen::ListMusic,
-            _ => app.quit(),
-        },
+        KeyCode::Esc => {
+            app.quit();
+        }
         // Exit application on `Ctrl-C`
         KeyCode::Char('c') | KeyCode::Char('C') => {
             if key_event.modifiers == KeyModifiers::CONTROL {
@@ -37,11 +38,11 @@ pub async fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<
 
         KeyCode::Up => handle_key_up(app),
         KeyCode::Down => handle_key_down(app),
+        KeyCode::Enter => enter_key(app).await,
         KeyCode::Char('k') => handle_key_up(app),
         KeyCode::Char('j') => handle_key_down(app),
-        KeyCode::Enter => enter_key(app).await,
+        KeyCode::Char('a') => handle_a(app),
         KeyCode::Char('d') => handle_delete_key(app),
-
         KeyCode::Char('p') => match app.screen_state {
             Screen::Playlist => {
                 play_and_download(app).await;
@@ -61,18 +62,8 @@ pub async fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<
             app.next_music();
         }
 
-        KeyCode::Char('a') => {
-            app.screen_state = Screen::InsertPlaylist;
-        }
-
         //add another
-        _ => match app.screen_state {
-            Screen::InsertPlaylist => {
-                app.input_playlist.handle_event(&Event::Key(key_event));
-            }
-
-            _ => {}
-        },
+        _ => {}
     }
     Ok(())
 }
