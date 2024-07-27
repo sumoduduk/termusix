6320577edad62718c6b349fffabd897d7e30bedd
sumoduduk <sumoduduk13@gmail.com>
2024-07-27 11:47:38 +0700

    feat(ui): import footer mod in ui

--- src/user_interface/app/ui.rs
+++ src/user_interface/app/ui.rs
@@ -1,7 +1,9 @@
+mod footer_components;
 mod music_list_components;
 mod playback_component;
 mod playlist_components;
 
+use footer_components::render_footer;
 use music_list_components::render_music_list;
 use playback_component::render_playback;
 use playlist_components::render_playlist;
@@ -17,15 +19,17 @@ use Constraint::*;
 const SELECTED_STYLE: Style = Style::new().bg(SLATE.c800).add_modifier(Modifier::BOLD);
 
 pub fn render(app: &mut App, area: Rect, buf: &mut Buffer) {
-    let horizontal_screen = Layout::horizontal([Percentage(35), Percentage(65)]);
+    let main_screen = Layout::vertical([Percentage(100), Min(3)]);
+    let [main_layout, footer_layout] = main_screen.areas(area);
 
-    let [main_layout, music_list_layout] = horizontal_screen.areas(area);
+    let horizontal_screen = Layout::horizontal([Percentage(35), Percentage(65)]);
+    let [main_layout, music_list_layout] = horizontal_screen.areas(main_layout);
+
     let main_frame = Layout::vertical([Percentage(50), Percentage(50)]);
     let [playback_layout, playlist_layout] = main_frame.areas(main_layout);
 
-    let song_name = app.get_now_playing();
-
-    render_playback(app, playback_layout, buf, song_name);
+    render_playback(app, playback_layout, buf);
     render_playlist(app, playlist_layout, buf);
-    render_music_list(app, music_list_layout, buf)
+    render_music_list(app, music_list_layout, buf);
+    render_footer(app, footer_layout, buf);
 }
