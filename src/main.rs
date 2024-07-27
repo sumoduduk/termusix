929b0949b8a0b84ef94bb62f11abcb56c105a948
sumoduduk <sumoduduk13@gmail.com>
2024-07-28 05:25:16 +0700

    chore: move app explicitly as import module

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@ mod playlist;
 mod user_interface;
 mod utils;
 
+use app::{screen::Screen, App};
 use handler::handle_key_events;
+use handler::input_playlist_handler::handle_key_input_events;
 use playback::start_playing;
 use ratatui::{backend::CrosstermBackend, Terminal};
 use serde::{Deserialize, Serialize};
@@ -44,7 +46,7 @@ async fn main() -> eyre::Result<()> {
     let now_playing: NowPlaying = Arc::new(RwLock::new(None::<String>));
     let now_playing_app = Arc::clone(&now_playing);
 
-    let mut app = app::App::new(tx, now_playing_app);
+    let mut app = App::new(tx, now_playing_app);
     // Initialize the terminal user interface.
     let backend = CrosstermBackend::new(io::stderr());
     let terminal = Terminal::new(backend)?;
@@ -60,7 +62,13 @@ async fn main() -> eyre::Result<()> {
         // Handle events.
         match tui.events.next().await? {
             Event::Tick => app.tick(),
-            Event::Key(key_event) => handle_key_events(key_event, &mut app).await?,
+            Event::Key(key_event) => match app.screen_state {
+                Screen::InsertPlaylist => handle_key_input_events(key_event, &mut app).await?,
+
+                _ => {
+                    handle_key_events(key_event, &mut app).await?;
+                }
+            },
             Event::Mouse(_) => {}
             Event::Resize(_, _) => {}
         }
