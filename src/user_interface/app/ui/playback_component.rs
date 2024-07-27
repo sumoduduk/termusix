248e68e4acecebc237b3e61494e88373c24af99a
sumoduduk <sumoduduk13@gmail.com>
2024-07-27 10:04:20 +0700

    feat(ui/playback_components) : create mod for screen playback ui

---
+++ src/user_interface/app/ui/playback_component.rs
@@ -0,0 +1,35 @@
+use ratatui::{
+    buffer::Buffer,
+    layout::{Alignment, Rect},
+    text::Text,
+    widgets::{Block, BorderType, Borders, Padding, Paragraph, Widget},
+};
+
+use crate::app::{App, Screen};
+
+pub fn render_playback(
+    app: &App,
+    playback_layout: Rect,
+    buf: &mut Buffer,
+    song_name: Option<String>,
+) {
+    let playback_block = Block::new()
+        .title("Now Playing")
+        .borders(Borders::ALL)
+        .padding(Padding::new(0, 0, playback_layout.height / 4, 0))
+        .border_type(BorderType::Rounded)
+        .border_style(app.get_border_color(Screen::Playback));
+
+    display_now_playing(song_name, playback_block, playback_layout, buf)
+}
+
+fn display_now_playing(song_name: Option<String>, block: Block, area: Rect, buf: &mut Buffer) {
+    let song_name = song_name.unwrap_or_default();
+
+    let text = Text::from(song_name).alignment(Alignment::Center);
+
+    Paragraph::new(text)
+        .block(block)
+        .centered()
+        .render(area, buf);
+}
