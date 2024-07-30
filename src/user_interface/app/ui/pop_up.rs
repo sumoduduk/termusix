faaf754582daee65c251517f526e62078f66ae6a
sumoduduk <sumoduduk13@gmail.com>
2024-07-30 10:41:09 +0700

    feat(pop up) : handle add song when screen popup

--- src/user_interface/app/ui/pop_up.rs
+++ src/user_interface/app/ui/pop_up.rs
@@ -1,4 +1,4 @@
-mod add_music_widget;
+pub mod add_music_widget;
 mod list_folder_add;
 mod pop_up_plylist;
 
@@ -6,16 +6,18 @@ use pop_up_plylist::render_popup_playlist;
 use ratatui::{
     buffer::Buffer,
     layout::{Constraint, Layout, Rect},
+    widgets::StatefulWidget,
 };
 
 use crate::{
     app::{screen::Screen, App},
-    user_interface::cursor::Cursor,
+    user_interface::cursor::AppState,
 };
 
-pub fn render_popup(app: &App, area: Rect, buf: &mut Buffer, cursor: &mut Cursor) {
-    match app.screen_state {
-        Screen::InsertPlaylist => render_popup_playlist(app, area, buf, cursor),
+pub fn render_popup(app: &mut App, area: Rect, buf: &mut Buffer, app_state: &mut AppState) {
+    match &app.screen_state {
+        Screen::InsertPlaylist => render_popup_playlist(app, area, buf, &mut app_state.cursor),
+        Screen::AddSongPopup(_add_screen) => app.add_song_popup.render(area, buf, app_state),
         _ => {}
     }
 }
