fc99a8d201c0e06de2ba55a926c2f186360af1d5
sumoduduk <sumoduduk13@gmail.com>
2024-07-30 10:46:11 +0700

    feat: add app state structt

--- src/user_interface/cursor.rs
+++ src/user_interface/cursor.rs
@@ -1,3 +1,5 @@
+use ratatui::widgets::ListState;
+
 #[derive(Default)]
 pub struct Cursor {
     position: Option<(u16, u16)>,
@@ -11,4 +13,16 @@ impl Cursor {
     pub fn take(&mut self) -> Option<(u16, u16)> {
         self.position.take()
     }
+}
+
+#[derive(Default)]
+pub struct AddSongListState {
+    pub file_scroll: ListState,
+    pub music_scroll: ListState,
+}
+
+#[derive(Default)]
+pub struct AppState {
+    pub cursor: Cursor,
+    pub add_song_state: AddSongListState,
 }
