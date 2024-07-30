e90e64db5147f087be3568827d64792b19ab1309
sumoduduk <sumoduduk13@gmail.com>
2024-07-31 02:56:11 +0700

    feat: change screen state after saving music to playlist

--- src/user_interface/handler/handle_enter.rs
+++ src/user_interface/handler/handle_enter.rs
@@ -5,6 +5,10 @@ pub async fn enter_key(app: &mut App) {
         Screen::Playlist => {
             app.screen_state = Screen::ListMusic;
         }
+        Screen::PopUpFileExplorer => {
+            app.save_song_to_playlist();
+            app.screen_state = Screen::ListMusic;
+        }
         _ => {}
     }
 }
