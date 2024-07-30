74f5b75cb92d957e2ca9795ab55fdfaa1aa31381
sumoduduk <sumoduduk13@gmail.com>
2024-07-31 02:55:12 +0700

    chore(pop up) : remove unused module

--- src/user_interface/app/ui/pop_up.rs
+++ src/user_interface/app/ui/pop_up.rs
@@ -1,9 +1,7 @@
-pub mod add_music_widget;
-mod list_folder_add;
 mod pop_up_plylist;
 
 use pop_up_plylist::render_popup_playlist;
-use ratatui::{buffer::Buffer, layout::Rect, widgets::StatefulWidget};
+use ratatui::{buffer::Buffer, layout::Rect};
 
 use crate::{
     app::{screen::Screen, App},
@@ -13,7 +11,6 @@ use crate::{
 pub fn render_popup(app: &mut App, area: Rect, buf: &mut Buffer, app_state: &mut AppState) {
     match &app.screen_state {
         Screen::InsertPlaylist => render_popup_playlist(app, area, buf, &mut app_state.cursor),
-        Screen::AddSongPopup(_add_screen) => app.add_song_popup.render(area, buf, app_state),
         _ => {}
     }
 }
