6977f6d7c83fdf55eae229a3a182ecbb70af8e49
sumoduduk <sumoduduk13@gmail.com>
2024-07-31 01:41:28 +0700

    feat(tui): implement render widget for file explorer pop up

--- src/user_interface/tui.rs
+++ src/user_interface/tui.rs
@@ -3,11 +3,18 @@ use crate::event::EventHandler;
 use ratatui::backend::Backend;
 use ratatui::crossterm::event::{DisableMouseCapture, EnableMouseCapture};
 use ratatui::crossterm::terminal::{self, EnterAlternateScreen, LeaveAlternateScreen};
+use ratatui::layout::{Constraint, Layout};
+use ratatui::widgets::Clear;
 use ratatui::Terminal;
 use std::io;
 use std::panic;
 
-use super::cursor::{AppState, Cursor};
+use super::app::centered_rect;
+use super::app::screen::Screen;
+use super::app::widget_list_add::{render_list_to_add, render_pop_up_top};
+use super::cursor::AppState;
+
+use Constraint::*;
 
 /// Representation of a terminal user interface.
 ///
@@ -55,7 +62,28 @@ impl<B: Backend> Tui<B> {
         self.terminal.draw(|frame| {
             let mut app_state = AppState::default();
 
-            frame.render_stateful_widget(app, frame.size(), &mut app_state);
+            if let Screen::PopUpFileExplorer = &app.screen_state {
+                let pop_up_area = centered_rect(80, 50, frame.size());
+
+                let top_bottom_area = Layout::vertical([Min(3), Percentage(100)]);
+
+                let [top_layout, botom_layout] = top_bottom_area.areas(pop_up_area);
+
+                let pop_up_top = render_pop_up_top();
+                frame.render_widget(pop_up_top, top_layout);
+
+                let main_pop_layout = Layout::horizontal([Percentage(50), Percentage(50)]);
+
+                let [file_layout, file_add_layout] = main_pop_layout.areas(botom_layout);
+
+                let add_widget = render_list_to_add(app);
+
+                frame.render_widget(add_widget, file_add_layout);
+
+                frame.render_widget(&app.file_explorer.widget(), file_layout);
+            } else {
+                frame.render_stateful_widget(app, frame.size(), &mut app_state);
+            }
 
             if let Some(position) = app_state.cursor.take() {
                 frame.set_cursor(position.0, position.1);
