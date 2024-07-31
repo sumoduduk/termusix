15774bbadf453307660df36fbb3bfa7b85f57382
sumoduduk <sumoduduk13@gmail.com>
2024-07-31 13:33:19 +0700

    feat(key_event) : create event keymap handler foor y to confirm deleteion of song and playlist

---
+++ src/user_interface/handler/handle_confirm.rs
@@ -0,0 +1,38 @@
+use crossterm::event::{KeyCode, KeyEvent};
+
+use crate::app::{screen::Screen, App};
+
+pub fn handle_y_key(app: &mut App) {
+    match app.screen_state {
+        Screen::Playlist => {
+            if let Some(playlist_index) = app.tabs_playlist.selected() {
+                app.playlist.delete_playlist(playlist_index);
+            }
+        }
+
+        Screen::ListMusic => {
+            if let Some(playlist_index) = app.tabs_playlist.selected() {
+                if let Some(song_index) = app.music_list.selected() {
+                    app.playlist.delete_song(playlist_index, song_index);
+                }
+            }
+        }
+
+        _ => {}
+    }
+}
+
+pub fn handle_confirm_popup(app: &mut App, key_code: KeyEvent) {
+    match key_code.code {
+        KeyCode::Char('y') => {
+            handle_y_key(app);
+            app.pop_up_confirm = false;
+        }
+
+        KeyCode::Char('n') => {
+            app.pop_up_confirm = false;
+        }
+
+        _ => {}
+    }
+}
