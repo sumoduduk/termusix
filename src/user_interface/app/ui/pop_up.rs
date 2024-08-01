f5991691652b7f2ff48dd798a5e02614c91dd39a
sumoduduk <sumoduduk13@gmail.com>
2024-08-01 12:19:15 +0700

    feat: render pop up help

--- src/user_interface/app/ui/pop_up.rs
+++ src/user_interface/app/ui/pop_up.rs
@@ -1,7 +1,9 @@
 mod confirm_popup;
+mod help_popup;
 mod pop_up_plylist;
 
 use confirm_popup::render_popup_confirm;
+use help_popup::render_help;
 use pop_up_plylist::render_popup_playlist;
 use ratatui::{buffer::Buffer, layout::Rect};
 
@@ -13,6 +15,7 @@ use crate::{
 pub fn render_popup(app: &mut App, area: Rect, buf: &mut Buffer, app_state: &mut AppState) {
     match &app.screen_state {
         Screen::InsertPlaylist => render_popup_playlist(app, area, buf, &mut app_state.cursor),
+        Screen::HelpPopup => render_help(app, area, buf),
         _ => {
             if app.pop_up_confirm {
                 render_popup_confirm(app, area, buf);
