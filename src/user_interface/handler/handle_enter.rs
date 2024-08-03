59bbebdfa5cedafe49e16b2737b60aaa54efabf2
sumoduduk <sumoduduk13@gmail.com>
2024-08-03 18:29:37 +0700

    feat: handle if single song played

--- src/user_interface/handler/handle_enter.rs
+++ src/user_interface/handler/handle_enter.rs
@@ -1,4 +1,4 @@
-use crate::app::{screen::Screen, App};
+use crate::{app::{screen::Screen, App}, playback::PlaybackEvent};
 
 pub async fn enter_key(app: &mut App) {
     match app.screen_state {
@@ -8,6 +8,25 @@ pub async fn enter_key(app: &mut App) {
         Screen::PopUpFileExplorer => {
             app.save_song_to_playlist();
             app.screen_state = Screen::ListMusic;
+        }
+        Screen::ListMusic => {
+            let Some(song_id) = app.music_list.selected() else {
+                return;
+            };
+
+            let sender = app.tx_playback.clone();
+            let _ = sender.send(PlaybackEvent::TrackPlay())
+
+            if let Ok(now_playing) = app.now_playing.read() {
+                match now_playing.playlist_id {
+                    Some(id) => {
+                        if let Some(index) = app.tabs_playlist.selected() {
+                            if id != index {}
+                        }
+                    }
+                    None => todo!(),
+                }
+            }
         }
         _ => {}
     }
