f7bb6e3cd12f9721a1dcfd1a21b3a8ac92c68bfb
sumoduduk <sumoduduk13@gmail.com>
2024-08-04 01:59:31 +0700

    feat(cwd): make music folder is the default fileexporer

--- src/user_interface/app/cwd_dirs.rs
+++ src/user_interface/app/cwd_dirs.rs
@@ -1,5 +1,17 @@
 use std::path::PathBuf;
 
 pub fn get_music_path() -> Option<PathBuf> {
-    dirs::audio_dir().or(dirs::home_dir())
+    let home_dir = dirs::home_dir()?;
+    let music_dir = home_dir.join("Music");
+
+    match music_dir.try_exists() {
+        Ok(is_exist) => {
+            if is_exist {
+                Some(music_dir)
+            } else {
+                dirs::home_dir()
+            }
+        }
+        Err(_) => dirs::home_dir(),
+    }
 }
