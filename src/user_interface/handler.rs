02ee0366b8d3680964d032166f33aece7f0c9611
sumoduduk <sumoduduk13@gmail.com>
2024-08-05 02:27:00 +0700

    feat: register + and - key event to handler + add char m key event for mute

--- src/user_interface/handler.rs
+++ src/user_interface/handler.rs
@@ -2,6 +2,7 @@ mod handle_confirm;
 mod handle_d;
 mod handle_enter;
 mod handle_left_right;
+mod handle_plus_minus;
 mod handle_space;
 mod handle_up_down;
 pub mod input_playlist_handler;
@@ -14,6 +15,7 @@ use handle_confirm::handle_confirm_popup;
 use handle_d::handle_delete_key;
 use handle_enter::enter_key;
 use handle_left_right::{hande_left, hande_right};
+use handle_plus_minus::{handle_minus, handle_plus};
 use handle_space::handle_space_key;
 use handle_up_down::{handle_key_down, handle_key_up};
 use insert_playlist_song::handle_a;
@@ -59,11 +61,16 @@ pub async fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<
         KeyCode::Right | KeyCode::Char('l') => {
             hande_right(app);
         }
+        KeyCode::Char('+') => handle_plus(app),
+        KeyCode::Char('-') => handle_minus(app),
         KeyCode::Up | KeyCode::Char('k') => handle_key_up(app),
         KeyCode::Down | KeyCode::Char('j') => handle_key_down(app),
         KeyCode::Enter => enter_key(app).await,
         KeyCode::Char('a') => handle_a(app),
         KeyCode::Char('d') => handle_delete_key(app),
+        KeyCode::Char('m') => {
+            app.mute_toggle();
+        }
         KeyCode::Char(' ') => {
             handle_space_key(app);
         }
