cec498c0761f4569d6c4f23e1171069ca815b284
sumoduduk <sumoduduk13@gmail.com>
2024-08-05 02:18:10 +0700

    fix(playlist): fix remove index song

--- src/playlist/delete_playlist.rs
+++ src/playlist/delete_playlist.rs
@@ -7,7 +7,7 @@ pub fn remove_playlist(playlist: &mut Playlist, index: usize) -> Option<()> {
 
 pub fn remove_song(playlist: &mut Playlist, index_id: usize, index_song: usize) -> Option<()> {
     let playlist_info = playlist.0.get_index_mut(index_id)?;
-    playlist_info.1.music_list.swap_remove_index(index_song)?;
+    playlist_info.1.music_list.shift_remove_index(index_song)?;
 
     Some(())
 }
