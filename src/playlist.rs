930a27485c6a248bba278ec4212469745a71b7e7
sumoduduk <sumoduduk13@gmail.com>
2024-08-05 02:30:50 +0700

    feat: add application directory for storing config

--- src/playlist.rs
+++ src/playlist.rs
@@ -7,7 +7,10 @@ use indexmap::IndexMap;
 use save_local::{save_local, save_local_music};
 use save_playlist::{get_playlist, save_file_json, save_id};
 use serde::{Deserialize, Serialize};
-use std::{fs, path::PathBuf};
+use std::{
+    fs::{self, create_dir_all},
+    path::{Path, PathBuf},
+};
 
 // #[derive(Serialize, Deserialize, Debug, PartialEq)]
 // struct MusicInfo {
@@ -26,54 +29,71 @@ struct InfoMusicPlaylist {
 type MusicPlaylist = IndexMap<String, InfoMusicPlaylist>;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Playlist(MusicPlaylist);
+pub struct Playlist(MusicPlaylist, PathBuf);
 
 impl Playlist {
     pub fn new() -> eyre::Result<Self> {
-        let raw = fs::read_to_string("music.json")?;
+        let dir_config = dirs::config_local_dir()
+            .expect("ERROR: No local config found")
+            .join("termusix");
 
+        if !dir_config.exists() {
+            let _ = create_dir_all(&dir_config);
+        }
+
+        let conf_file = dir_config.join("music.json");
+
+        if !conf_file.exists() {
+            Self::create_config_file(&conf_file).expect("Create Config File Failed");
+        }
+
+        let raw = fs::read_to_string(&conf_file)?;
         let data: MusicPlaylist = serde_json::from_str(&raw)?;
-
-        let playlist = Playlist(data);
+        let playlist = Playlist(data, conf_file);
 
         Ok(playlist)
     }
 
     pub async fn save_playlist(&mut self, url: &str) -> eyre::Result<()> {
         get_playlist(self, url).await?;
-        save_file_json(&self.0)?;
+        save_file_json(&self.0, &self.1)?;
         Ok(())
     }
 
     pub fn save_local_playlist(&mut self, id: &str) -> eyre::Result<()> {
         save_local(self, id);
 
-        save_file_json(&self.0)?;
+        save_file_json(&self.0, &self.1)?;
         Ok(())
     }
 
     pub fn save_local_song(&mut self, song_list: &[PathBuf], index_id: usize) -> eyre::Result<()> {
         save_local_music(self, song_list, index_id);
 
-        save_file_json(&self.0)?;
+        save_file_json(&self.0, &self.1)?;
+        Ok(())
+    }
+
+    fn create_config_file(path_file: &Path) -> eyre::Result<()> {
+        fs::write(path_file, "{}")?;
         Ok(())
     }
 
     pub async fn save_by_id(&mut self, id: &str) -> eyre::Result<()> {
         save_id(self, id).await?;
-        save_file_json(&self.0)?;
+        save_file_json(&self.0, &self.1)?;
         Ok(())
     }
 
     pub fn delete_playlist(&mut self, index_id: usize) {
         if remove_playlist(self, index_id).is_some() {
-            let _ = save_file_json(&self.0);
+            let _ = save_file_json(&self.0, &self.1);
         }
     }
 
     pub fn delete_song(&mut self, index_id: usize, index_song: usize) {
         if remove_song(self, index_id, index_song).is_some() {
-            let _ = save_file_json(&self.0);
+            let _ = save_file_json(&self.0, &self.1);
         }
     }
 
