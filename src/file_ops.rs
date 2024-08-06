1ebb45162c01d5f24b43010b59e76b5d360cadbd
sumoduduk <sumoduduk13@gmail.com>
2024-08-06 13:34:19 +0700

    feat: remove unused function and import

--- src/file_ops.rs
+++ src/file_ops.rs
@@ -1,11 +1,7 @@
 use eyre::{eyre, Context};
 use rodio::Decoder;
-use std::{
-    fs::{metadata as metada_sync, File},
-    path::Path,
-    sync::mpsc::Sender,
-};
-use tokio::fs::{metadata, read_dir};
+use std::{fs::File, path::Path, sync::mpsc::Sender};
+use tokio::fs::read_dir;
 
 use crate::playback::PlaybackEvent;
 
@@ -50,51 +46,3 @@ pub fn extract_extentions(path: &Path) -> Option<&str> {
 pub fn get_parent(path: &Path) -> Option<&Path> {
     path.parent()
 }
-
-pub async fn check_file_exist(file_name: &str) -> Option<&str> {
-    let meta_res = metadata(file_name).await;
-    match meta_res {
-        Ok(meta_res) => {
-            let size = meta_res.len();
-            let is_file = meta_res.is_file();
-
-            if size > 0 && is_file {
-                Some(file_name)
-            } else {
-                None
-            }
-        }
-        Err(_) => None,
-    }
-}
-
-pub fn check_file_exist_sync(file_name: &str) -> Option<&str> {
-    let meta_res = metada_sync(file_name);
-    match meta_res {
-        Ok(meta_res) => {
-            let size = meta_res.len();
-            let is_file = meta_res.is_file();
-
-            if size > 0 && is_file {
-                Some(file_name)
-            } else {
-                None
-            }
-        }
-        Err(_) => None,
-    }
-}
-
-// pub async fn count_file(folder_path: &str) -> eyre::Result<usize> {
-//     let mut count = 0;
-//
-//     let mut dir = fs::read_dir(folder_path).await?;
-//     while let Some(entry) = dir.next_entry().await? {
-//         let file_type = entry.file_type().await?;
-//         if file_type.is_file() {
-//             count += 1;
-//         }
-//     }
-//
-//     Ok(count)
-// }
