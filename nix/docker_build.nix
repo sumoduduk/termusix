bbe9f1b95433f5b9eb627124ac8d5d5ac7b6c286
sumoduduk <sumoduduk13@gmail.com>
2024-08-14 02:25:15 +0700

    feat: add docker build

--- nix/docker_build.nix
+++ nix/docker_build.nix
@@ -5,22 +5,30 @@
 }: let
   inherit (inputs) nixpkgs;
   pkgs = nixpkgs.legacyPackages.${localSystem};
+
+  entry_script = pkgs.writeText ''
+    PCM_CARD=${PCM_CARD:-0}
+    CTL_CARD=${CTL_CARD:-0}
+
+    echo "Configuring ALSA with PCM card $ALSA_PCM_CARD and CTL card $ALSA_CTL_CARD"
+    echo "pcm.!default {
+        type hw
+        card $PCM_CARD
+    }
+
+    ctl.!default {
+        type hw
+        card $CTL_CARD
+    }" > /etc/asound.conf
+
+    # Execute the provided command
+    exec "$@"
+  '';
 in
-  pkgs.dockerTools.buildLayeredImage {
+  pkgs.dockerTools.buildImage {
     name = "termusix";
-    tag = "0.1.0";
+    tag = "latest";
     config = {
       Entrypoint = ["${termusix}/bin/termusix"];
-      Env = [
-        "PATH=${pkgs.coreutils}/bin:${pkgs.bash}/bin:${pkgs.alsa-lib}/bin:${pkgs.dbus}/bin"
-      ];
     };
-    contents = [
-      pkgs.coreutils
-      pkgs.bash
-      pkgs.alsa-lib
-      pkgs.dbus
-      pkgs.alsa-utils
-      termusix
-    ];
   }
