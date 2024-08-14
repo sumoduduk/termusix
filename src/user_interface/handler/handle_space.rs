88217d054b652f21c67bb0a4156e7dae25b70cc4
sumoduduk <sumoduduk13@gmail.com>
2024-08-14 14:34:50 +0700

    feat(key handle) : make pause toggle global all screen

--- src/user_interface/handler/handle_space.rs
+++ src/user_interface/handler/handle_space.rs
@@ -2,13 +2,11 @@ use crate::app::{screen::Screen, App};
 
 pub fn handle_space_key(app: &mut App) {
     match &app.screen_state {
-        Screen::Playback => {
-            app.pause_toggle();
-        }
-
         Screen::PopUpFileExplorer => {
             app.push_song_from_explorer();
         }
-        _ => {}
+        _ => {
+            app.pause_toggle();
+        }
     }
 }
